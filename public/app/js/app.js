fa08af0fa3d11f47be6bd0ef679325e8d33c1b1d
Austin Moffa <austinmoffa@gmail.com>
2016-02-02 22:25:43 -0500

    Might as well release this nonsense then

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -2,4 +2,24 @@ angular.module('mainApp', ['ngRoute', 'ngMaterial', 'appRoutes', 'MainCtrl', 'Bl
 .config(function($mdThemingProvider) {
     $mdThemingProvider.theme('default')
         .primaryPalette('grey')
-});
+})
+.run(['$rootScope', '$route', function($rootScope, $route) {
+    $rootScope.$on('$routeChangeSuccess', function() {
+        var titles = [
+            'I am not the Walrus',
+            'Worst thing Since Waterworld',
+            'Surplus Robot Limbs',
+            'My Mom Likes My Site',
+            'Crunchy',
+            'All Content Ethically Sourced',
+            'Flatland 2: Earth',
+            'Schopenhauer\'s Dream Book',  
+            'Deep Couch City',
+        ];
+
+        var title = titles[Math.floor(Math.random()*titles.length)];
+
+        document.title = title;
+    });
+}]);
+
