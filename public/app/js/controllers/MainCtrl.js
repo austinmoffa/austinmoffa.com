cc5bd8798c06588c0044c5d1ff29a8ae09b14ec6
Austin Moffa <austinmoffa@gmail.com>
2016-08-10 13:21:38 -0400

    Deleting

--- public/app/js/controllers/MainCtrl.js
+++
@@ -1,8 +0,0 @@
-angular.module('MainCtrl', []).controller('MainController', function($scope, $mdSidenav) {
-    $scope.main_nav = 'app/templates/main_nav.html';
-    $scope.navOpen = false;
-
-    $scope.openNav = function() {
-        $mdSidenav('main_nav').open();
-    }
-});
